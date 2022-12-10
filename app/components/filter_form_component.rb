9e9c147a37b342a5e11e5023609f26d72b72f1a1
ptphong1925 <ptphong1925@gmail.com>
2022-12-11 00:07:31 +0700

    update

--- app/components/filter_form_component.rb
+++ app/components/filter_form_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class FilterFormComponent < ViewComponent::Base
-  def initialize(url:, method:)
+  def initialize(url:, method: :get)
     @url = url
     @method = method
   end
