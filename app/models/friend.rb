0ef28baa25fcf8b6fd3b66d48e498ebfbba4eb25
ptphong1925 <ptphong1925@gmail.com>
2022-12-23 11:13:35 +0700

    update

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -10,10 +10,17 @@ class Friend < ApplicationRecord
     case1 || case2
   end
 
-  def self.friend?(person1, person2)
+  def self.friend_together?(person1, person2)
     case1 = person1.friendables.where(accepted?: true).pluck(:friendable_type ,:friendable_id).include?([person2.class.name, person2.id])
     case2 = person1.frienders.where(accepted?: true).pluck(:friender_type ,:friender_id).include?([person2.class.name, person2.id])
     case1 || case2
+  end
+
+  def self.friends_of(person)
+    friends = []
+    array_of_Friend = Friend.where(friender_type: person.class.name, friender_id: person.id, accepted?: true).pluck(:friendable_type, :friendable_id)
+    array_of_Friend = array_of_Friend + Friend.where(friendable_type: person.class.name, friendable_id: person.id, accepted?: true).pluck(:friender_type, :friender_id)
+    array_of_Friend.map { |class_name, id| class_name.constantize.find(id) }
   end
 
   def friend_uniqueness
@@ -23,5 +30,6 @@ class Friend < ApplicationRecord
       errors.add("Friend is reacted!")
     end
   end
+
 
 end
