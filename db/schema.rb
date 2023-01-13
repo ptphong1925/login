2bd0396510c3f40cb2610fc2ccb7a65233929a99
ptphong1925 <ptphong1925@gmail.com>
2023-01-13 12:24:12 +0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_053000) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_052349) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -225,6 +225,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_053000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "last_seen_at"
+    t.integer "uid"
+    t.string "provider"
   end
 
   create_table "versions", force: :cascade do |t|
